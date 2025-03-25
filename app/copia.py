f0dae35bc014ec4e691e3e08ff5e46d05ab18ade
Jose Pineda Serrano <josepise@correo.ugr.es>
2025-03-25 13:08:24 +0100

    Pruebas varias de Translator

--- app/copia.py
+++ app/copia.py
@@ -4,8 +4,6 @@ import sympy as sp
 import os
 
 class SimulationModelGenerator(ABC):
-    
-
     def __init__(self, equations, conditionals, name_file):
         self.equations = equations
         self.conditionals = conditionals
@@ -15,46 +13,32 @@ class SimulationModelGenerator(ABC):
     def generate_file(self):
         pass
 
-
-    #def check_components(self):
-        
-
-# Hacemos una clase que genere el archivo de simulación continua en python.
-
 class PythonSimulationGenerator(SimulationModelGenerator):
-
     def __init__(self, equations, conditionals, name_file):
         super().__init__(equations, conditionals, name_file)
     
-
     def generate_file(self):
-        
-        #Creamos el archivo de simulación continua en python en la carpeta 
-        # ../models/name_file.py.
+        # Creamos el archivo de simulación continua en python en la carpeta ../models/name_file.py.
         os.makedirs("./models/", exist_ok=True)
         self.file = open(f"./models/{self.name_file}.py", "w")
 
-        #Escribimos el archivo.
+        # Escribimos el archivo.
         self.write_head_file()
         self.write_model_parameters()
         self.write_equations()
         self.write_numerical_methods()
         self.write_main()
-        self.plot_results()
+        self.file.close()
 
     def write_head_file(self):
-        #Añadimos los imports necesarios.
+        # Añadimos los imports necesarios.
         self.file.write("import numpy as np\n")
         self.file.write("import matplotlib.pyplot as plt\n")
         self.file.write("import sympy as sp\n")
         self.file.write("\n\n")
 
     def write_model_parameters(self):
-        
-        #Añadimos una constante que contenga el número de ecuaciones.
-        self.file.write("n_equations = " + str(len(self.equations)) + "\n")
-
-        #Añadimos las constantes de las ecuaciones y sus valores.
+        # Añadimos las constantes de las ecuaciones y sus valores.
         self.file.write("# Parámetros del modelo\n")
         for equation in self.equations:
             constant_values = equation.get_constants_values()
@@ -64,80 +48,72 @@ class PythonSimulationGenerator(SimulationModelGenerator):
         self.file.write("\n\n")
 
         # Añadimos la variable que cuenta los pasos de tiempo
-        self.file.write("paso = 0\n")
+        self.file.write("i = 0\n")
         # Crear una lista basada en el número de ecuaciones
         n = len(self.equations[0].get_simbol())
         list_str = f"[{', '.join(['[]' for _ in range(n)])}]"
 
         self.file.write("est = " + list_str + "\n")
-        self.file.write("f = " + list_str + "\n")
         self.file.write("\n\n")
 
-        #Añadimos las constantes de inicio y fin de la simulación.
+        # Añadimos las constantes de inicio y fin de la simulación.
         self.file.write("t0 = 0\n")
         self.file.write("tf = 10\n")
         self.file.write("dt = 0.01\n")
         self.file.write("\n\n")
 
-    def write_initialization_method(self):
-        self.file.write("def initialize():\n")
-        self.file.write("    paso = 0\n")
-        # Crear una lista basada en el número de ecuaciones
-        n = len(self.equations[0].get_simbol())
-        list_str = f"[{', '.join(['[]' for _ in range(n)])}]"
-
-        self.file.write("est = " + list_str + "\n")
-        self.file.write("f = " + list_str + "\n")
-        self.file.write("\n\n")
-
-
     def write_equations(self):
+        # Escribimos la cabecera de la función que contendrá las ecuaciones.
+        self.file.write("def deriv(t):\n")
+        self.file.write("    global i\n")
+        self.file.write("    if i == 0:\n")
+        self.file.write("        est[0].append(0)\n")
+        self.file.write("        est[1].append(0)\n")
+        self.file.write("    else:\n")
 
-        #Escribimos la cabecera de la función que contendrá las ecuaciones.
-        self.file.write("def deriv():\n")
-
-        #Añadimos las ecuaciones al archivo.
-        for i,equation in enumerate(self.equations):
-            #Sustituimos los simbolos por la cadena est[i] para poder evaluar la ecuación.
-
+        # Añadimos las ecuaciones al archivo.
+        for i, equation in enumerate(self.equations):
+            # Sustituimos los símbolos por la cadena est[i] para poder evaluar la ecuación.
             symbols = equation.get_simbol()
-            subs_dict = {str(sym): sp.Symbol(f'est[{j}][paso-1]') for j, sym in enumerate(symbols)}
- 
+            subs_dict = {str(sym): sp.Symbol(f'est[{j}][i-1]') for j, sym in enumerate(symbols)}
             # Realizamos la sustitución con el diccionario generado
             eq = equation.get_equation().subs(subs_dict)
-            self.file.write(f"\tf[{i}].append({eq})\n")
+            self.file.write(f"        est[{i}].append({eq})\n")
+        self.file.write("    i += 1\n")
+        self.file.write("    return [est[0][-1], est[1][-1]]\n")
         self.file.write("\n")
 
-
     def write_numerical_methods(self):
-
-        #Añadimos el metodo de Euler para resolver las ecuaciones.
-        self.file.write("def one_step_euler(tt, hh):\n")
-        self.file.write("    deriv(tt)\n")
-        self.file.write("    for i in range(n_equations):\n")
-        self.file.write("        f.append(est[i][paso-1] + (hh * f[i][paso-1]))\n")
+        # Añadimos el método de Euler para resolver las ecuaciones.
+        self.file.write("def one_step_euler(inp, tt, hh):\n")
+        self.file.write("    f = deriv(tt)\n")
+        self.file.write("    out = [0] * len(inp)\n")
+        self.file.write("    for j in range(len(inp)):\n")
+        self.file.write("        out[j] = inp[j] + (hh * f[j])\n")
+        self.file.write("    return out\n")
         self.file.write("\n\n")
-
-
 
     def write_main(self):
         self.file.write("def main():\n")
         self.file.write("    t = np.arange(t0, tf, dt)\n")
+        self.file.write("    x = np.zeros((len(t), 2))\n")
+        self.file.write("    x[0] = [0, 0]  # Initial conditions\n")
         self.file.write("\n\n")
         self.file.write("    for i in range(1, len(t)):\n")
-        self.file.write("        paso += 1\n")
-        self.file.write("        one_step_euler(t[i-1], dt)\n")
+        self.file.write("        x[i] = one_step_euler(x[i-1], t[i-1], dt)\n")
         self.file.write("\n\n")
-
-    def plot_results(self):
+        self.file.write("    return t, x\n")
+        self.file.write("\n\n")
         self.file.write("if __name__ == '__main__':\n")
-        self.file.write("    main()\n")
-        self.file.write("    plt.plot(t, est[0], label='x')\n")
-        self.file.write("    plt.plot(t, est[1], label='y')\n")
+        self.file.write("    t, x = main()\n")
+        self.file.write("    plt.plot(t, x[:, 0], label='x')\n")
+        self.file.write("    plt.plot(t, x[:, 1], label='y')\n")
+        self.file.write("    plt.xlabel('Time')\n")
+        self.file.write("    plt.ylabel('Values')\n")
+        self.file.write("    plt.legend()\n")
         self.file.write("    plt.show()\n")
-        self.file.write("\n\n")
-    
-    
+
+# Ejemplo de uso
 eq = Equation()
 eq.add_equation("a*x-b*x*y", 'x y', 'a b ', {"a": 5, "b": 0.2})
 eq.process_equations()
@@ -147,7 +123,3 @@ eq2.process_equations()
 equations = [eq, eq2]
 
 PythonSimulationGenerator(equations, [], "simulation").generate_file()
-
-
-
-    
--- models/simulation.py
+++ models/simulation.py
@@ -2,34 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
 
-
+n_equations = 2
 # Parámetros del modelo
-a = 1
-b = 2
-c = 3
-d = 4
-e = 5
-f = 6
+a = 5
+b = 0.2
+c = 0.05
+d = 0.0004
 
-
-i = 0
+paso = 0
 est = [[], []]
-
+f = [[], []]
 
 t0 = 0
-tf = 10
+tf = 1000
 dt = 0.01
+t = np.arange(t0, tf, dt)
 
+def deriv():
+    global paso
+    f[0].append(a * est[0][paso-1] - b * est[0][paso-1] * est[1][paso-1])
+    f[1].append(c * est[0][paso-1] * est[1][paso-1] - d * est[1][paso-1])
 
-def deriv(t):
-	est[0].append(a*est[0][i]**2 + b*est[0][i] + c*est[1][i])
-	est[1].append(d*est[0][i]**2 + e*est[0][i] + est[1][i]*f)
+def one_step_euler(tt, hh):
+    deriv()
+    for i in range(n_equations):
+        print(est[i][paso-1] + (hh * f[i][paso-1]))
+        est[i].append(est[i][paso-1] + (hh * f[i][paso-1]))
+def main():
+    global paso
+    # Inicializamos las condiciones iniciales
+    est[0].append(d/c)  # Condición inicial para x
+    est[1].append(a/b)   # Condición inicial para y
 
-def one_step_euler(inp, tt, hh):
-    f = deriv(tt)
-    out = [0] * len(inp)
-    for i in range(len(inp)):
-        out[i] = inp[i] + (hh * f[i])
-    return out
+    for i in range(1, len(t)):
+        paso += 1
+        print(est[0][paso-1], est[1][paso-1])
+        one_step_euler(t[i], dt)
+
+if __name__ == '__main__':
+    main()
+    plt.plot(t, est[0], label='x')
+    plt.plot(t, est[1], label='y')
+    plt.xlabel('Time')
+    plt.ylabel('Values')
+    plt.legend()
+    plt.show()
 
 
